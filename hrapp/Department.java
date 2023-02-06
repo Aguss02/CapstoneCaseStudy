1ae078676eaa5f9f90327b44abb65410cfd5eb8a
Aguss02 <tosi.agustina02@gmail.com>
2023-02-06 14:30:03 -0300

    First Commit

---
+++ hrapp/Department.java
@@ -0,0 +1,79 @@
+package hrapp;
+
+public class Department {
+
+    private String name;
+    private Employee[] employees = new Employee[10];
+
+    private int lastAddedEmployeeIndex = -1;
+
+    public Department(){
+    }
+
+    public Department(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Employee[] getEmployees() {
+        return employees;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Department: " + name;
+    }
+
+    public void addEmployee (Employee employee){
+        if (lastAddedEmployeeIndex < employees.length) {
+            lastAddedEmployeeIndex++;
+            employees[lastAddedEmployeeIndex] = employee;
+        }
+    }
+    
+    public Employee[] getAllEmployees(){
+        Employee[] actualEmployees = new Employee[lastAddedEmployeeIndex+1];
+        for (int i = 0; i < actualEmployees.length; i++){
+            actualEmployees[i] = employees[i];
+        }
+        return actualEmployees;
+    }
+
+    public Employee getEmployeeById(int id){
+        for (Employee employee: employees
+        ) {
+            if (employee.getID() == id){
+                return employee;
+            }
+    }
+        return null;
+    }
+
+    public int getNumberOfEmployees(){
+       return lastAddedEmployeeIndex+1;
+    }
+
+    public double getTotalSalary(){
+       double total = 0.0;
+        for (int i = 0; i <= lastAddedEmployeeIndex; i++){
+            total += employees[i].getSalary();
+        }
+        return total;
+    }
+
+    public double getAverageSalary(){
+        if (lastAddedEmployeeIndex > -1){
+            return getTotalSalary() / (lastAddedEmployeeIndex+1);
+        }
+        return 0.0;
+    }
+
+
+}
